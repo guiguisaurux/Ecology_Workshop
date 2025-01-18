bde9cefe0b27a147427fea8d796c322cc07556fb
Guillaume Saint-Jacques <195578437+guiguisaurux@users.noreply.github.com>
2025-01-18 15:14:24 -0500

    créer fichier

---
+++ analyse.R
@@ -0,0 +1,8 @@
+#charger packages
+library(tidyverse)
+library(ratdat)
+
+#graphique
+ggplot(data = complete_old, aes(x = weight, y = hindfoot_length)) +
+  geom_point(color = "blue") +
+  geom_line()
